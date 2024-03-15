880adf7305538725410177de72445aa2337e1ed9
root user1 <root@doamin.com>
2024-03-15 14:00:11 -0400

    Added RAID to diskconfig.sh

--- diskconfig.sh
+++ diskconfig.sh
@@ -4,13 +4,13 @@
 FILENAME=`hostname`
 echo "Disk report saved to $FILENAME.report"
 
-echo -e "/n LVM Configuration: \n\n" >>$file.report
+echo -e "/n LVM Configuration: \n\n" >>$FILENAME.report
 lvscan >>$FILENAME.report
 
-echo -e "\n\n Partition Configuration: \n\n" >>$file.report
+echo -e "\n\n Partition Configuration: \n\n" >>$FILENAME.report
 fdisk -l | head -17 >>$FILENAME.report
 
-echo -e "\n\n Mounted Filesystems: \n\n" >>$file.report
+echo -e "\n\n Mounted Filesystems: \n\n" >>$FILENAME.report
 
 df -hT | grep -v tmp >>$FILENAME.report
 
